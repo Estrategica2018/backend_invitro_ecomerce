a6340654f65290a40e423e309d27e229f9d2e84d
cristian <cristianjojoa01@gmail.com>
2022-08-29 19:32:16 -0500

    servicios resultado del cruce de productos

--- app/Http/Controllers/ProductResultCrossController.php
+++ app/Http/Controllers/ProductResultCrossController.php
@@ -2,20 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\ProductResultCross\StoreProductResultCross;
+use App\Http\Requests\ProductResultCross\UpdateProductResultCross;
 use App\Models\ProductResultCross;
 use Illuminate\Http\Request;
 
 class ProductResultCrossController extends Controller
 {
     //
-    public function store()
+    public function store(StoreProductResultCross $request)
     {
-
+        $productResultCross = ProductResultCross::create($request->all());
+        return response()->json([
+            'overall_status' => 'successfull',
+            'message' => '¡Resultado del cruce de productos registrado exitosamente!',
+            'data' => [
+                'product_result_cross' => $productResultCross,
+            ]
+        ],201);
     }
 
-    public function update()
+    public function update(UpdateProductResultCross $request, ProductResultCross $productResultCross)
     {
-
+        $productResultCross->update($request->all());
+        return response()->json([
+            'overall_status' => 'successfull',
+            'message' => '¡Resultado del cruce de productos actualizado exitosamente!',
+            'data' => [
+                'product' => $productResultCross,
+            ]
+        ],201);
     }
 
     public function list()
